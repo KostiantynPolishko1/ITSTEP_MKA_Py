60f5ef907757f42d3b81ea5d5c589d355756c400
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-04-20 06:30:51 +0300

    3. Created class Student.

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import random
 
 
-class Coin:
-    xvalue = 0
+class Student:
+    position = 0
+    name = ''
+    age = 0
+    count = 0
 
-    def __init__(self):
-        self.xvalue = 1
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Student.count += 1
+        self.position = self.count
+
+    def info(self) -> None:
+        print(f'{self.position} | {self.name} | {self.age}')
+
+    def date(self) -> str:
+        return f'{self.position} | {self.name} | {self.age}'
+
+    def __del__(self):
+        print(f'deleted obj {self.date()}')
 
 
-coin = Coin()
-print(coin.xvalue)
+Kota = Student('Kost', 25)
+Mare = Student('Maxim', 18)
+
+# print(Kota.name)
+# print(Mare.name)
+
+Kota.info()
+print(Mare.date())
+print(Student.count)
